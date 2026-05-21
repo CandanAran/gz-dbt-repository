434457e2c5ea22c60c206ac51392d89db1a6237c
arancandan@gmail.com <arancandan@gmail.com>
2026-05-21 13:41:45 +0000

    sales_margin model created

---
+++ models/Intermediate/Int_sales_margin.sql
@@ -0,0 +1,11 @@
+SELECT 
+s.*,
+p.products_id,
+p.purchase_price,
+ROUND(s.quantity * p.purchase_price, 2) AS purchase_cost,
+ROUND(s.revenue * (s.quantity * p.purchase_price), 2) AS margin
+
+FROM {{ ref('stg_raw__sales') }} s
+
+LEFT JOIN {{ ref('stg_raw__product') }} p
+    ON s.products_id = p.products_id

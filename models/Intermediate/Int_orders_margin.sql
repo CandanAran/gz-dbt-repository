b18bbb78b9f882648ccdad326d4b629b13481304
arancandan@gmail.com <arancandan@gmail.com>
2026-05-21 14:04:54 +0000

    orders_margin created

---
+++ models/Intermediate/Int_orders_margin.sql
@@ -0,0 +1,10 @@
+SELECT
+orders_id,
+MIN(date_date) AS date_date,
+ROUND(SUM(revenue), 1) AS revenue,
+SUM(quantity) AS quantity,
+ROUND(SUM(purchase_cost), 1) AS purchase_cost,
+ROUND(SUM(margin), 1) AS margin
+
+FROM {{ ref('Int_sales_margin') }}
+GROUP BY orders_id
